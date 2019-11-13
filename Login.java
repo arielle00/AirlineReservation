41a2f0f7ccedf0c5529993ec743512642bece4ee
Arielle Shnayder <Arielle00@gmail.com>
2019-11-13 14:06:28 -0800

    Add files via upload

--- Login.java
+++ Login.java
@@ -3,27 +3,36 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 public class Login {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		JFrame frame = new JFrame();
 		frame.setSize(500, 300);
-		WindowUtilities.setNativeLookAndFeel(); //sets the look and feel with the Window Utilities class
-
 		
 		JPanel panel = new JPanel();
 		panel.setLayout(null);
 		
 		JLabel title = new JLabel("Airline Reservation");
+		title.setFont(title.getFont().deriveFont(30.0f));
 		JLabel username = new JLabel("Username");
 		JLabel password = new JLabel("Password");
 		JButton enter = new JButton("Login");
@@ -31,7 +40,7 @@ public class Login {
 		JTextField user = new JTextField(15);
 		JTextField pass = new JTextField(15);
 	
-		title.setBounds(160, 10, 200, 100);
+		title.setBounds(85, 10, 300, 100);
 		username.setBounds(100, 100, 150, 21);
 		user.setBounds(250, 100, 100, 21);
 		password.setBounds(100, 140, 100, 21);
@@ -54,20 +63,37 @@ public class Login {
 			String passWord = pass.getText();
 			
 			if (userName.compareTo(passWord)==0) {
-				//JFrame ticketFrame = new JFrame();
-				//ticketFrame.setSize(500, 300);
-				//ticketFrame.setVisible(true);
-                                frame.remove(panel);
-                                frame.repaint();
-                                frame.revalidate();
+				frame.remove(panel);
+				frame.repaint();
+				frame.revalidate();
+
+				SelectFlight object = null;
+				try {
+					object = new SelectFlight();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				try {
+					frame.add(object.makeButtons());
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				frame.setVisible(true);
+				
 			}
+			
+			else
+			{
+				JOptionPane.showMessageDialog(null, "Invalid username or password. Try again");
+	 /*prints this message when the username and/or password is wrong*/
+			}
+
 		});
-		
-		
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		panel.setBackground(Color.GRAY);
-	    //frame.pack();
 	    frame.setVisible(true);
 
 	}
